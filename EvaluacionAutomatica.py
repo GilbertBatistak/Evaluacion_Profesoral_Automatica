4fa0237cd6e51761636f2079a78156537459f98c
GilbertBatistak <GilbertBatista.k@gmail.com>
2020-03-23 13:14:26 -0400

    AutoCompleter function "finished"

--- EvaluacionAutomatica.py
+++ EvaluacionAutomatica.py
@@ -25,11 +25,31 @@ class Bot():
     
     def profCheck(self):
         while 1:
-            i = 0
+            i = 1
             try:
                 profC = bot.driver.find_element_by_xpath('/html/body/div[1]/table/tbody/tr[{0}]/td[7]/button'.format(i)).click()
+                completeEval()
+                i += 1
             except:
-                print("Completed")
+                print("Completed!")
+                break
+
+    def completeEval(self):
+        while 1:
+            i = 1
+            try:
+                calif = bot.driver.find_element_by_xpath('/html/body/div[2]/form/div[2]/div/div[2]/table/tbody/tr[{0}]/td[4]/label'.format(i)).click
+                i += 1
+            except:
+                try:
+                    nextBut = bot.driver.find_element_by_xpath('/html/body/div[2]/form/div[3]/button[2]').click
+                except:
+                    try:
+                        comment = bot.driver.find_element_by_xpath('//*[@id="Respuestas_17__OpcionValorText"]')
+                        comment.send_keys("Excelente profesor!")
+                    except:
+                        print("Completed!")
+                        break
 
 bot = Bot()
 bot.login()
